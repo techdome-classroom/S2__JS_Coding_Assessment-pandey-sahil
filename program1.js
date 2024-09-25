ebae0880b0350b035e938c16f3fc0e16ecbc5b0d
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2024-09-25 16:15:51 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,6 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = [];
+
+    const matchingBrackets = {
+        ')':'(',
+        '}':'{',
+        ']':'['
+    };
+
+    for (let char of s) {
+        if (char in matchingBrackets) {
+            let topElement = stack.length > 0 ? stack.pop() : "#";
+
+            if (topElement !== matchingBrackets[char]){
+                return false;
+
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
     
 };
 
