9b8c213a1f11c8029d5e51d4b408888816deb370
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2024-09-25 16:16:23 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    if(s.length === 0){
+        return 0;
+    }
+
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    const length = s.length;
+
+    for(let i = 0; i < length; i++) {
+        const currentVal = romanMap[s[i]];
+        const nextVal = i + 1 < length ? romanMap[s[i + 1]] : 0;
+
+        if (currentVal < nextVal){
+            total -= currentVal;
+        } else {
+            total += currentVal;
+        }
+    }
+    return total;
 };
 
 
